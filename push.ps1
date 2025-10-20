f54db46418b383c89d670fdf138905ec72ed1ba8
pat600130 <pat600130@gmail.com>
2025-10-20 15:51:44 -0400

    updated gitignore

--- push.ps1
+++ push.ps1
@@ -1,7 +1,4 @@
 git add .
-if ($args.Length -eq 0) {
-    Write-Host "Please provide a commit message."
-    exit 1
-}
+if ($args.Length -eq 0) {    Write-Host "Please provide a commit message."    exit 1}
 git commit -m $args[0]
 git push 
