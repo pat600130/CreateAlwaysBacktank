82c422abbb208339fc84a9bdf0349dfe6ef32682
pat600130 <pat600130@gmail.com>
2026-04-24 16:08:45 -0400

    idfk

--- neoforge/src/main/java/net/pat600/neoforge/mixin/MixinRemainingAirOverlay.java
+++ neoforge/src/main/java/net/pat600/neoforge/mixin/MixinRemainingAirOverlay.java
@@ -3,7 +3,8 @@ package net.pat600.neoforge.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.createmod.catnip.gui.element.GuiGameElement;
-import net.createmod.catnip.utility.theme.Color;
+//import net.createmod.catnip.utility.theme.Color;
+import net.pat600.neoforge.ColorCopy;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -14,6 +15,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.util.StringUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameType;
+
+import com.simibubi.create.content.equipment.armor.BacktankUtil;
 
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -28,21 +31,29 @@ public class MixinRemainingAirOverlay implements LayeredDraw.Layer {
         if (!mc.options.hideGui && mc.gameMode.getPlayerMode() != GameType.SPECTATOR) {
             LocalPlayer player = mc.player;
             if (player != null) {
-                int timeLeft = player.getPersistentData().getInt("VisualBacktankAir");
-                PoseStack poseStack = guiGraphics.pose();
-                poseStack.pushPose();
-                ItemStack backtank = getDisplayedBacktank(player);
-                poseStack.translate((float)(guiGraphics.guiWidth() / 2 + 90), (float)(guiGraphics.guiHeight() - 53 + (backtank.has(DataComponents.FIRE_RESISTANT) ? 9 : 0)), 0.0F);
-                Component text = Component.literal(StringUtil.formatTickDuration(Math.max(0, timeLeft - 1) * 20, mc.level.tickRateManager().tickrate()));
-                GuiGameElement.of(backtank).at(0.0F, 0.0F).render(guiGraphics);
-                int color = -1;
-                if (timeLeft < 60 && timeLeft % 2 == 0) {
-                    color = Color.mixColors(-65536, color, Math.max((float)timeLeft / 60.0F, 0.25F));
+                int timeLeft =0;
+                int maxAir =0;
+                for (int i = 0; i < BacktankUtil.getAllWithAir(player).size(); i++) {
+                    ItemStack backtank = BacktankUtil.getAllWithAir(player).get(i);
+                    timeLeft+=BacktankUtil.getAir(backtank);
+                    maxAir+=BacktankUtil.maxAir(backtank);
                 }
+                if (timeLeft>0||maxAir<0) {
+                    PoseStack poseStack = guiGraphics.pose();
+                    poseStack.pushPose();
+                    ItemStack backtank = getDisplayedBacktank(player);
+                    poseStack.translate((float)(guiGraphics.guiWidth() / 2 + 90), (float)(guiGraphics.guiHeight() - 53 + (backtank.has(DataComponents.FIRE_RESISTANT) ? 9 : 0)), 0.0F);
+                    Component text = Component.literal(StringUtil.formatTickDuration(Math.max(0, timeLeft - 1) * 20, mc.level.tickRateManager().tickrate()));
+                    GuiGameElement.of(backtank).at(0.0F, 0.0F).render(guiGraphics);
+                    int color = -1;
+                    if (timeLeft < 60 && timeLeft % 2 == 0) {
+                        color = ColorCopy.mixColors(-65536, color, Math.max((float)timeLeft / 60.0F, 0.25F));
+                    }
 
-                guiGraphics.drawString(mc.font, text, 16, 5, color);
-                poseStack.popPose();
+                    guiGraphics.drawString(mc.font, text, 16, 5, color);
+                    poseStack.popPose();
                 }
-            }
+            }}
+
     }
 }
